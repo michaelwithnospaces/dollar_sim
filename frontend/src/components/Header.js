1c67e6a6599398104cbe9f9fc95402b54ea3c60f
Daniel-Lam1702 <lamdaniel1702@gmail.com>
2024-01-27 22:48:33 -0600

    react js with figma generated code

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,129 @@
+const Header = () => {
+    return (
+    <div style={{width: '100%', height: '100%', position: 'relative', background: '#86A7FC', borderRadius: 88, overflow: 'hidden'}}>
+    <div style={{width: 2124.75, height: 1328, left: -580, top: -363, position: 'absolute', opacity: 0.15}}>
+        <div style={{width: 2063, height: 1327.81, left: 61.67, top: 0, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 1461.93, height: 1223.90, left: 569.54, top: 103.90, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 1276.43, height: 766.96, left: 717.08, top: 560.86, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 506.94, height: 1327.80, left: 0, top: 0.05, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 548.80, height: 551.24, left: 346.94, top: 776.58, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 262.24, height: 1256.38, left: 1862.43, top: 71.42, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 499.59, height: 549.44, left: 106.30, top: 0, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 648.01, height: 338.63, left: 894.68, top: 989.17, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 863.14, height: 404.79, left: 1246.57, top: 0.05, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 405.57, height: 465.97, left: 150.51, top: 0.05, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 277.46, height: 582.53, left: 0.02, top: 161.03, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 381.77, height: 254.96, left: 1606.77, top: 0.05, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 306.04, height: 277.44, left: 198.59, top: 0.05, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 506.28, height: 314.24, left: 1550.27, top: 0.05, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 210.02, height: 570.94, left: 1914.61, top: 136.13, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 145.49, height: 378.38, left: 1979.14, top: 237.42, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 88.20, height: 408.28, left: 0.02, top: 919.50, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 194.20, height: 166.79, left: 1673.16, top: 0.03, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 62.24, height: 386.90, left: 2062.51, top: 941.10, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 90.93, height: 305.37, left: 0.03, top: 340.28, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 280.53, height: 129.88, left: 926.23, top: 1197.92, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 124.42, height: 117.86, left: 411.76, top: 1209.94, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 222.41, height: 65.89, left: 1303.01, top: 1261.92, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 215.77, height: 98.56, left: 954.45, top: 1229.24, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 162.66, height: 61.29, left: 1789.35, top: 1266.51, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 147.72, height: 64.37, left: 985.99, top: 1263.43, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 150.96, height: 53.06, left: 1022.51, top: 0.05, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 39.18, height: 189.21, left: 0.03, top: 1075.52, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 29.07, height: 122.82, left: 2095.58, top: 396.71, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 15.20, height: 82.99, left: 0.03, top: 419.95, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 46.48, height: 23.20, left: 793.45, top: 1304.60, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 8.22, height: 94.59, left: 2116.41, top: 1123.65, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 49.79, height: 14.28, left: 1034.12, top: 1313.52, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 34.86, height: 5.40, left: 1392.64, top: 1322.39, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 348.98, height: 465.79, left: 560.97, top: 16.06, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 449.71, height: 487.50, left: 124.37, top: 807.11, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 328.35, height: 281.47, left: 1226.55, top: 97.91, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 246.25, height: 265.69, left: 852.05, top: 621.88, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 162.72, height: 213.25, left: 1450.69, top: 479.52, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 63.11, height: 67.97, left: 1668.31, top: 811.43, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 398.44, height: 398.33, left: 766.79, top: 164.43, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 121.17, height: 224.76, left: 1845.83, top: 716.76, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 115.82, height: 103.68, left: 1696.12, top: 592.50, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 150.62, height: 163.40, left: 1595.40, top: 1037.95, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 135.03, height: 88.81, left: 1061.09, top: 1029.47, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 178.92, height: 228.36, left: 623.47, top: 859.03, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 167.93, height: 140.32, left: 1324.89, top: 1037.37, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 162.51, height: 149.29, left: 265.75, top: 53.38, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 110.44, height: 169.16, left: 720.78, top: 104.32, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 368.09, height: 319.50, left: 157.03, top: 858, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 203.53, height: 175.38, left: 1309.34, top: 155.78, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 54.81, height: 64.25, left: 977.75, top: 737.37, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 31.33, height: 37.95, left: 1512.43, top: 576.44, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 264.10, height: 258.61, left: 828.13, top: 247.98, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 230.70, height: 210.32, left: 194.03, top: 912.25, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 34.07, height: 29.29, left: 1401.68, top: 230.76, position: 'absolute', border: '2px white solid'}}></div>
+        <div style={{width: 82.49, height: 79.21, left: 250.74, top: 979.71, position: 'absolute', border: '2px white solid'}}></div>
+    </div>
+    <div style={{width: 750, height: 1000, left: 0, top: 624, position: 'absolute', background: '#FFDD95', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}></div>
+    <div style={{width: 750, height: 88, left: 0, top: 0, position: 'absolute'}}>
+        <div style={{width: 48.66, height: 22.67, left: 672, top: 34.67, position: 'absolute'}}>
+            <div style={{width: 44, height: 22.67, left: 0, top: 0, position: 'absolute', opacity: 0.35}}>
+                <div style={{width: 44, height: 22.67, left: 0, top: 0, position: 'absolute', opacity: 0.35}}></div>
+                <div style={{width: 42, height: 20.67, left: 1, top: 1, position: 'absolute', opacity: 0.35, border: '2px white solid'}}></div>
+            </div>
+            <div style={{width: 2.66, height: 8, left: 46, top: 7.33, position: 'absolute', opacity: 0.40, background: 'white'}}></div>
+            <div style={{width: 36, height: 14.67, left: 4, top: 4, position: 'absolute', background: 'white'}}></div>
+        </div>
+        <div style={{width: 30.55, height: 21.93, left: 631.39, top: 34.66, position: 'absolute', background: 'white'}}></div>
+        <div style={{width: 34, height: 21.33, left: 587.33, top: 35.33, position: 'absolute', background: 'white'}}></div>
+        <div style={{left: 63.79, top: 26.66, position: 'absolute', color: 'white', fontSize: 30, fontFamily: 'SF Pro Text', fontWeight: '600', wordWrap: 'break-word'}}>9:41</div>
+    </div>
+    <div style={{left: 320, top: 200, position: 'absolute', color: 'white', fontSize: 64, fontFamily: 'Work Sans', fontWeight: '400', wordWrap: 'break-word'}}>Hi!</div>
+    <div style={{left: 247, top: 546, position: 'absolute', color: 'white', fontSize: 48, fontFamily: 'Actor', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>transactions</div>
+    <div style={{left: 149, top: 275, position: 'absolute', color: 'white', fontSize: 64, fontFamily: 'Work Sans', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>Your balance is</div>
+    <div style={{width: 437, height: 175, left: 154, top: 350, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 115, fontFamily: 'Work Sans', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>$15,615</div>
+    <div style={{width: 750, height: 205, left: 0, top: 1419, position: 'absolute', background: '#86A7FC'}}>
+        <div style={{width: 268, height: 10, left: 240, top: 177, position: 'absolute', opacity: 0.20, background: 'white', borderRadius: 200}} />
+        <div style={{width: 64, height: 64, paddingTop: 8, paddingBottom: 6.70, paddingLeft: 6.67, paddingRight: 7.91, left: 60, top: 40, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+            <div style={{width: 49.43, height: 49.30, position: 'relative'}}>
+                <div style={{width: 49.43, height: 22, left: 0, top: 0, position: 'absolute', border: '4px #3468C0 solid'}}></div>
+                <div style={{width: 42.07, height: 30.63, left: 3.68, top: 18.67, position: 'absolute', border: '4px #3468C0 solid'}}></div>
+                <div style={{width: 15.27, height: 21, left: 17.08, top: 27.06, position: 'absolute', border: '4px #3468C0 solid'}}></div>
+            </div>
+        </div>
+        <div style={{width: 64, height: 64, paddingTop: 8, paddingBottom: 6.67, paddingLeft: 12, paddingRight: 12.67, left: 201.50, top: 40, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+            <div style={{width: 39.33, height: 49.33, position: 'relative'}}>
+                <div style={{width: 39.33, height: 49.33, left: 0, top: 0, position: 'absolute', border: '4px white solid'}}></div>
+                <div style={{width: 33.33, height: 0, left: 4.67, top: 43.33, position: 'absolute', border: '4px white solid'}}></div>
+                <div style={{width: 0, height: 36.67, left: 8.67, top: 0.67, position: 'absolute', border: '4px white solid'}}></div>
+            </div>
+        </div>
+        <div style={{width: 64, height: 64, left: 343, top: 40, position: 'absolute'}} />
+        <div style={{width: 64, height: 64, left: 484.50, top: 40, position: 'absolute'}} />
+        <div style={{width: 64, height: 64, paddingTop: 8, paddingBottom: 6.67, paddingLeft: 8, paddingRight: 6.67, left: 626, top: 40, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+            <div style={{width: 49.33, height: 49.33, position: 'relative'}}>
+                <div style={{width: 49.33, height: 49.33, left: 0, top: 0, position: 'absolute', border: '4px white solid'}}></div>
+                <div style={{width: 17.33, height: 17.33, left: 16, top: 5.33, position: 'absolute', border: '4px white solid'}}></div>
+                <div style={{width: 35.60, height: 20.67, left: 6.87, top: 28.67, position: 'absolute', border: '4px white solid'}}></div>
+            </div>
+        </div>
+        <div style={{width: 16, height: 16, left: 650, top: 114, position: 'absolute', background: 'white', borderRadius: 9999}} />
+        <div style={{width: 109, height: 21, left: 45, top: 114, position: 'absolute', color: '#3468C0', fontSize: 22, fontFamily: 'Work Sans', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>Finances</div>
+        <div style={{width: 109, height: 21, left: 179, top: 114, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 22, fontFamily: 'Work Sans', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>Jobs</div>
+        <div style={{width: 109, height: 21, left: 321, top: 111, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 22, fontFamily: 'Work Sans', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>Chat</div>
+        <div style={{width: 109, height: 21, left: 461, top: 109, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 22, fontFamily: 'Work Sans', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>Store</div>
+        <div style={{width: 82, height: 82, left: 334, top: 26, position: 'absolute'}}>
+            <img style={{width: 54.67, height: 60.26, left: 13.67, top: 13.67, position: 'absolute', border: '3px white solid'}} src="https://via.placeholder.com/55x60" />
+            <div style={{width: 6.83, height: 6.83, left: 51.25, top: 34.17, position: 'absolute', background: 'white', borderRadius: 9999, border: '1px white solid'}} />
+            <div style={{width: 6.83, height: 6.83, left: 37.58, top: 34.17, position: 'absolute', background: 'white', borderRadius: 9999, border: '1px white solid'}} />
+            <div style={{width: 6.83, height: 6.83, left: 23.92, top: 34.17, position: 'absolute', background: 'white', borderRadius: 9999, border: '1px white solid'}} />
+        </div>
+        <div style={{width: 77, height: 77, left: 477, top: 27, position: 'absolute'}}>
+            <div style={{width: 16.04, height: 25.67, left: 25.67, top: 25.67, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', borderRadius: 44, border: '2px white solid'}}></div>
+            <div style={{width: 0, height: 6.42, left: 48.12, top: 38.50, position: 'absolute', borderRadius: 2, border: '2px white solid'}}></div>
+            <div style={{width: 0, height: 6.42, left: 28.88, top: 38.50, position: 'absolute', borderRadius: 2, border: '2px white solid'}}></div>
+            <div style={{width: 51.33, height: 41.71, left: 12.83, top: 25.67, position: 'absolute', border: '3px white solid'}}></div>
+        </div>
+    </div>
+    <img style={{width: 214, height: 154, left: 20, top: 80, position: 'absolute'}} src="https://via.placeholder.com/214x154" />
+    </div>
+    );
+}
+
+export default Header;
