9044a6f71df3edbf8bad38d3c7ff894d6a0aa088
Cindy191 <Cindy.lam191@gmail.com>
2024-01-28 07:39:28 -0600

    frontend work

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -21,13 +21,11 @@ const Navbar = () => {
                     <a href="#">Link 2</a>
                 </div> */}
                 <label for="dog-names"></label> 
-                <select name="dog-names" id="dog-names"> 
+                <select name="dog-names" id="dog-names" onchange="location = this.value;"> 
                     <option value="Parent">Parent</option> 
-                    <option value="Job-Post-Form">Job-Post-Form</option> 
+                    <option value="/jobPostingForm/">Job-Post-Form</option>
                     <option value="AddItem">AddItem</option>    
                 </select>
-  
-
             </li>
         </ul>
     </nav>
